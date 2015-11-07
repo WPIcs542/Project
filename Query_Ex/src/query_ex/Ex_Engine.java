9e35841b6c52cb165874c2b3897a994957d25e70
Jonathan0 <johnlin989@gmail.com>
2015-11-07 17:05:53 -0500

    create db files
    Jonathan Lin

--- Query_Ex/src/query_ex/Ex_Engine.java
+++ Query_Ex/src/query_ex/Ex_Engine.java
@@ -1,5 +1,11 @@
 package query_ex;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * CS542 Project_3
  * @author Fangyu Lin; Hongzhang Cheng; Zhaojun Yang
@@ -7,6 +13,48 @@ package query_ex;
  */
 
 public class Ex_Engine {
+	Relation city = new Relation("city.db");
+	Relation country = new Relation("country.db");
+	String cityfile = "city.txt";
+	String countryfile = "country.txt";
+	
+	public Ex_Engine(){}
+	
+	public void init() throws IOException{
+		String cityfile = "city.txt";
+		String countryfile = "country.txt";
+		FileInputStream fs;
+		InputStreamReader isr;
+		BufferedReader b_read;
+		String templine;
+		try {
+			fs = new FileInputStream(cityfile);
+			isr = new InputStreamReader(fs);
+			b_read = new BufferedReader(isr);
+			while((templine=b_read.readLine()) != null){
+				city.put(templine.getBytes().hashCode(), templine.getBytes());
+			}
+			city.saveContents();
+			b_read.close();
+			isr.close();
+			fs.close();
+			
+			fs = new FileInputStream(countryfile);
+			isr = new InputStreamReader(fs);
+			b_read = new BufferedReader(isr);
+			while((templine=b_read.readLine()) != null){
+				country.put(templine.getBytes().hashCode(), templine.getBytes());
+			}
+			country.saveContents();
+			b_read.close();
+			isr.close();
+			fs.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Initialization of \"city.db\" and \"country.db\" successful!");
+	}
 	
 	public void open(){
 		
