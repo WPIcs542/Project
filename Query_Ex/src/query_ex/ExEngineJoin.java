10e432f401057a47d111e20c4b29d20f653a26cd
hcheng3 <hcheng3@wpi.edu>
2015-11-11 11:50:56 -0500

    join

--- Query_Ex/src/query_ex/ExEngineJoin.java
+++ Query_Ex/src/query_ex/ExEngineJoin.java
@@ -5,6 +5,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+
 
 /**
  * CS542 Project_3
@@ -16,10 +20,12 @@ import java.io.InputStreamReader;
 public class ExEngineJoin {
 	Relation city;
 	Relation country;
+	
 	String cityfile = "city.txt";
 	String countryfile = "country.txt";
 	// to store result and to be used in PROJECT operator
 	private Relation joinResult = new Relation("joinResult.db");
+	
 
 	public ExEngineJoin() {
 	}
@@ -72,11 +78,55 @@ public class ExEngineJoin {
 	}
 
 	// put the whole joined tuple into joinResult
-	public void getNext() {
-
+	public void getNext()  {	
+	String[] tumpleofcity = null;
+	String [] tumpleofcountry = null;
+	String cjc ="";
+	String cityjoincountry[]=null;
+	
+	Enumeration<byte[]> enumofcity =city.getValuesEnum();
+	Enumeration<byte[]> enumofcountry= country.getValuesEnum();
+	while(enumofcity.hasMoreElements()){
+	  try{
+		  tumpleofcity =splitoftuple(enumofcity.nextElement());
+	  }catch(UnsupportedEncodingException exp){
+		  exp.printStackTrace();
+	  }
+	      while(enumofcountry.hasMoreElements()){
+	    	  try{
+	    	      tumpleofcountry =splitoftuple(enumofcountry.nextElement());
+	    	  }catch (UnsupportedEncodingException exp){
+	    		exp.printStackTrace();  
+	    	  }
+	    	  if(tumpleofcity[0].equals(tumpleofcountry[2])){	    		
+	    		  Double cityP = Double.parseDouble(tumpleofcity[4]);
+                  Double countryP=Double.parseDouble(tumpleofcountry[6]);
+                  if(cityP/countryP>0.4){
+                	  cityjoincountry = new String[tumpleofcity.length+tumpleofcountry.length];
+	                  System.arraycopy(tumpleofcity,0,cityjoincountry,0, tumpleofcity.length);
+	                  System.arraycopy(tumpleofcountry,0,cityjoincountry,tumpleofcity.length , tumpleofcountry.length);
+		    		     for(int i=0;i<cityjoincountry.length-1;i++){    		          
+		    		    	 cjc +=  cityjoincountry[i] + "," ;  
+	    		         }	
+		    		     cjc+=cityjoincountry[cityjoincountry.length-1];
+	    	         }   	    
+	    	    joinResult.put(cjc.getBytes().hashCode(),cjc.getBytes()); }
+	    	
+	      }
+	
+	  }
+	
 	}
+	
+	public String[]splitoftuple(byte[] tuple) throws UnsupportedEncodingException {
+		String str = new String(tuple, StandardCharsets.UTF_8);
+		return str.split(","); // ignores commas inside quotation marks
+	    }
+
+	
+	
 
 	public void close() {
-		// joinResult.saveContents();
+		joinResult.saveContents();
 	}
 }
